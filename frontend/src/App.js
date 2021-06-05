00ef3c4199092f21065544a6d95ccd81c1498198
Ujjwal Krishna <ujjwalkrishna7@gmail.com>
2021-06-05 16:03:27 +0530

    added a working frontend and backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to my site</h1>
-    </div>
+    <Router>
+      <Header></Header>
+      <main className="py-3">
+        <Container>
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer></Footer>
+    </Router>
   );
 }
 
