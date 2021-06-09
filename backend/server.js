df8f7f78dc524b8b98f72250fc9d7b033a42cede
Ujjwal Krishna <ujjwalkrishna7@gmail.com>
2021-06-09 12:58:44 +0530

    added the backend for user authentication

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,22 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import colors from "colors";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 connectDB();
 
 const app = express();
 
+app.use(express.json()); //body parser
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 //404 Error handling middleware
 app.use(notFound);
