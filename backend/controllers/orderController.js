4dab93f62f4c81775863d4a19730335436c2fb03
Ujjwal Krishna <ujjwalkrishna7@gmail.com>
2021-06-11 13:53:20 +0530

    added order screen, my orders and paypal payment

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -55,4 +55,36 @@ const getOrderbyId = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderbyId };
+//@desc Update order to paid
+//@route GET /api/orders/:id/pay
+//@access Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not Found");
+  }
+});
+
+//@desc Get logged in user orders
+//@route GET /api/orders/myorders
+//@access Private
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id });
+
+  res.json(orders);
+});
+
+export { addOrderItems, getOrderbyId, updateOrderToPaid, getMyOrders };
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,16 @@
 import express from "express";
 const router = express.Router();
-import { addOrderItems, getOrderbyId } from "../controllers/orderController.js";
+import {
+  addOrderItems,
+  getOrderbyId,
+  updateOrderToPaid,
+  getMyOrders,
+} from "../controllers/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(protect, addOrderItems);
+router.route("/myorders").get(protect, getMyOrders);
 router.route("/:id").get(protect, getOrderbyId);
+router.route("/:id/pay").put(protect, updateOrderToPaid);
+
 export default router;
